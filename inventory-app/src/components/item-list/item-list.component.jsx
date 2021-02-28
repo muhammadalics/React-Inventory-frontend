7dde9a3b71afa6eb96c4d47a690434c695798cef
Muhammad Ali <muhammadali.pete@gmail.com>
2021-02-27 20:57:07 -0700

    DELETE is now working

--- inventory-app/src/components/item-list/item-list.component.jsx
+++ inventory-app/src/components/item-list/item-list.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {Item} from '../item/item.component';
+import  Item  from '../item/item.component';
 
 
 import './item-list.styles.scss'
@@ -23,11 +23,19 @@ class ItemList extends React.Component {
         const resp = await fetch(url);
         const data = await resp.json();
         console.log(data);
+        
         this.setState({ items: data, loading: false })
+        console.log(this.state.items._id);
 
 
     }
 
+    async handleDelete() {
+        alert('You are deleting this item!')
+        const response = await fetch('http://localhost:5000/api/products/', {
+            method: 'DELETE'
+        })
+    }
 
     render() {
 
@@ -40,10 +48,10 @@ class ItemList extends React.Component {
                 ) : (
                         <div className="item-list">
                             {
-                                this.state.items.map(({ department, name, size, corrosionfree, material, weight}) => {
+                                this.state.items.map(({ department, name, size, corrosionfree, material, weight, _id}) => {
                                     return <div>
-                                        <Item department={department} name={name} size={size} corrosionfree={corrosionfree} material={material} weight={weight} />
-                                        </div>
+                                        <Item department={department} name={name} size={size} corrosionfree={corrosionfree} material={material} weight={weight} id={_id} OnClickX={this.handleDelete} />
+                                    </div>
 
                                 })
                             }
--- inventory-app/src/components/item/item.component.jsx
+++ inventory-app/src/components/item/item.component.jsx
@@ -3,14 +3,92 @@ import React from 'react';
 import './item.styles.css'
 
 
-export const Item = (props) => (
-    <div>
+// export const Item = (props) => (
+//     <div className="img-div">       
         
-        <img className="image" src={props.department}/>
-        <div>{props.name}</div>
-        <div>{props.size}</div>
-        <div>{props.corrosionfree}</div>
-        <div>{props.material}</div>
-        <div>{props.weight}</div>
+//         <div className="span" onClick={props.OnClickX}>X</div> 
+//         {/* {<div className="span" onClick={handleDelete(props.id)}>X</div>  }
+//         <div className="span">X</div> */}
+//         <img className="image" src={props.department}/>
+
+//         <div>{props.name}</div>
+//         <div>{props.size}</div>
+//         <div>{props.corrosionfree}</div>
+//         <div>{props.material}</div>
+//         <div>{props.weight}</div>
+//     </div>
+// )
+
+// async function handleDelete(id) {
+//     alert('You are deleting this item!')
+//     const response = await fetch('http://localhost:5000/api/products/' + {id}, {
+//         method: 'DELETE'
+//     })
+// }
+
+class Item extends React.Component{
+    constructor({department, name, size, corrosionfree, material, weight, id}){
+        super({department, name, size, corrosionfree, material, weight, id});
+
+        this.state = {
+            department:department,
+            name:name,
+            size:size, 
+            corrosionfree:corrosionfree, 
+            material:material, 
+            weight:weight,
+            id:id
+        }
+    }
+
+// componentDidMount(){
+//     this.setState(
+//         {
+//             department:{department},
+//             name:name,
+//             size:size, 
+//             corrosionfree:corrosionfree, 
+//             material:material, 
+//             weight:weight,
+//             id:id
+//         }
+
+//     )
+// }
+
+
+
+a = async handleDelete => {
+    // alert('You are deleting this item!')
+    
+    console.log(this.state.department)
+    
+    const response = await fetch('http://localhost:5000/api/products/' + this.state.id, {
+        method: 'DELETE'
+    })
+}
+
+render(){
+    return(
+        <div className="img-div">       
+        
+        {/* <div className="span" onClick={props.OnClickX}>X</div>  */}
+        <div className="span" onClick={this.a}>X</div> 
+        {/* {<div className="span" onClick={handleDelete(props.id)}>X</div>  }
+        <div className="span">X</div> */}
+        <img className="image" src={this.state.department}/>
+
+        <div>{this.state.name}</div>
+        <div>{this.state.size}</div>
+        <div>{this.state.corrosionfree}</div>
+        <div>{this.state.material}</div>
+        <div>{this.state.weight}</div>
     </div>
-)
+
+
+    )
+}
+
+}
+
+export default Item;
