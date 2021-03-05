0f4781e4476c6d7962b51fa06ff336f9db1b5efa
Muhammad Ali <muhammadali.pete@gmail.com>
2021-03-04 22:10:54 -0700

    updated browserrouter basename to reflect deployed url address.

--- inventory-app/src/index.js
+++ inventory-app/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter basename ="/React-Inventory-frontend">
+  <BrowserRouter basename ="/crud-app">
   {/* <BrowserRouter> */}
   <React.StrictMode>
     <App />
